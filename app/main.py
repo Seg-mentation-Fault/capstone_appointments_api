4e65f9b955c09730db72e7aee34d5b9bf4f8d3d1
Louvani <paula.louvani@gmail.com>
2022-09-15 20:47:38 -0500

    Add cors

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 
 from app.config.generate_credentials import generate_credentials
 from app.routers import appointments
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 generate_credentials()
 
